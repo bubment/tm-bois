0b5ee451bb6f732b307551f135329a57a95976bc
Schin Ádám <schin.adam@gmail.com>
2022-04-15 14:42:09 +0200

    Frontend init

---
+++ public/app.js
@@ -0,0 +1,53 @@
+
+const dropArea = document.querySelector(".drag-area");
+const dragText = dropArea.querySelector("header");
+const button = dropArea.querySelector("button");
+const input = dropArea.querySelector("input");
+
+let file;
+
+button.onclick = () => {
+    input.click();
+}
+
+input.addEventListener("change", function () {
+    file = this.files[0];
+    dropArea.classList.add("active");
+    showFile();
+});
+
+dropArea.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    dropArea.classList.add("active");
+    dragText.textContent = "Release to Upload File";
+});
+
+dropArea.addEventListener("dragleave", () => {
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+});
+
+dropArea.addEventListener("drop", (event) => {
+    event.preventDefault();
+    file = event.dataTransfer.files[0];
+    showFile();
+});
+
+function showFile() {
+    let fileType = file.type;
+    let validExtensions = ["gbx"];
+    if (validExtensions.includes(fileType)) {
+        let fileReader = new FileReader(); //creating new FileReader object
+        fileReader.onload = () => {
+            let fileURL = fileReader.result; //passing user file source in fileURL variable
+            // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
+            // let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
+            dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+        }
+        fileReader.readAsDataURL(file);
+    } else {
+        alert("This is not an GBX File!");
+        dropArea.classList.remove("active");
+        dragText.textContent = "Drag & Drop to Upload File";
+    }
+}
