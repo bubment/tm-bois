3965279d7251b69c117adf1edbb0acafa5e769e4
Schin Ádám <schin.adam@gmail.com>
2022-04-15 19:18:03 +0200

    DnD

--- public/drag-and-drop.js
+++ public/drag-and-drop.js
@@ -3,22 +3,83 @@ const textAreaEmpty = document.querySelector(".area-empty");
 const textAreaFilled = document.querySelector(".area-filled");
 const filledText = textAreaFilled.querySelector("header");
 const dragText = dropArea.querySelector("header");
-const button = dropArea.querySelector("button");
+const browseButton = textAreaEmpty.querySelector("button");
+const uploadButton = textAreaFilled.querySelector("button");
 const input = dropArea.querySelector("input");
 
 
-button.onclick = () => {
-    input.click();
+var GBXArray = [];
+
+function transformFiles(fileList) {
+    for (file of fileList) {
+        const reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.onloadend = async (event) => {
+            if (event.target.readyState === FileReader.DONE) {
+                const arrayBuffer = event.target.result;
+                const array = new Uint8Array(arrayBuffer);
+
+                let mGBX = new GBX({
+                    data: array,
+                    onParse: function (metadata) {
+                        if (metadata.type == "Replay") {
+                            GBXArray.push(metadata);
+                        }
+                    }
+                })
+            }
+        }
+    }
 }
 
-input.addEventListener("change", function () {
+function acceptFiles(fileList) {
+    var isValidExtension = true;
+    for (file of fileList) {
+        let fileType = file.name.split(".").pop();
+        let validExtensions = ["Gbx"];
+        if (!validExtensions.includes(fileType)) {
+            alert("Only GBX files are allowed!");
+            isValidExtension = false;
+        }
+    }
+
+    if (isValidExtension) {
+        transformFiles(fileList);
+        filledText.textContent = `${fileList.length} file ready to upload`;
+        textAreaEmpty.style.display = "none";
+        textAreaFilled.style.display = "flex";
+    } else {
+        dragText.textContent = "Drag & Drop to Upload File";
+    }
+}
+
+browseButton.addEventListener("click", () => {
+    input.click();
+});
+
+uploadButton.addEventListener("click", () => {
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(GBXArray)
+    };
+    fetch(`${HOST}/replays`, requestOptions)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+});
+
+input.addEventListener("change", () => {
     var fileList = this.files;
+    dropArea.classList.remove("active");
     acceptFiles(fileList);
 });
 
 dropArea.addEventListener("dragover", (event) => {
+    GBXArray = [];
     event.preventDefault();
     dropArea.classList.add("active");
+    textAreaEmpty.style.display = "flex";
+    textAreaFilled.style.display = "none";
     dragText.textContent = "Release to Upload File";
 });
 
@@ -30,48 +91,7 @@ dropArea.addEventListener("dragleave", () => {
 dropArea.addEventListener("drop", (event) => {
     event.preventDefault();
     var fileList = event.dataTransfer.files;
+    dropArea.classList.remove("active");
     acceptFiles(fileList);
 });
 
-function acceptFiles(fileList) {
-    console.log(fileList)
-
-    for (file of fileList) {
-        let fileType = file.name.split(".").pop();
-        let validExtensions = ["Gbx"];
-        if (!validExtensions.includes(fileType)) {
-            alert("Only GBX files are allowed!");
-
-        }
-    }
-
-    dropArea.classList.remove("active");
-    filledText.textContent = `${fileList.length} file ready to upload`;
-    textAreaEmpty.style.display = "none";
-    textAreaFilled.style.display = "flex";
-}
-
-function handleFile(fileList) {
-
-    if (validExtensions.includes(fileType)) {
-        const reader = new FileReader();
-        reader.readAsArrayBuffer(file);
-        reader.onloadend = (event) => {
-            if (event.target.readyState === FileReader.DONE) {
-                const arrayBuffer = event.target.result;
-                const array = new Uint8Array(arrayBuffer);
-
-                let mGBX = new GBX({
-                    data: array,
-                    onParse: function (metadata) {
-                        console.log(metadata)
-                    }
-                })
-            }
-        }
-    } else {
-        alert("This is not a GBX File!");
-        dropArea.classList.remove("active");
-        dragText.textContent = "Drag & Drop to Upload File";
-    }
-}
