b0f4aa91135a920a7115f4add385c0f89f4e7c5d
bubment <medgyesiattila@elektromont.hu>
2022-04-17 20:54:07 +0200

    Tipps and tricks

--- public/drag-and-drop.js
+++ public/drag-and-drop.js
@@ -1,6 +1,9 @@
+// [TIPP #10] class helyet érdemesebb inkább id (vagy valami unique azonosító) alapján hivatkozni a html elemekre
 const dropArea = document.querySelector(".drag-area");
 const textAreaEmpty = document.querySelector(".area-empty");
 const textAreaFilled = document.querySelector(".area-filled");
+// [TIPP #11] Nem érdemes szülő már az elején több beágyazott rétegen keresztül hivatkozni elemekre
+// Ha bárki belenyúl a kódba simán eltörheti a hivatkozásokat és máris nem működik a kód.
 const filledText = textAreaFilled.querySelector("header");
 const emptyText = textAreaEmpty.querySelector("header");
 const browseButton = textAreaEmpty.querySelector("button");
@@ -18,7 +21,7 @@ function transformFiles() {
             if (event.target.readyState === FileReader.DONE) {
                 const arrayBuffer = event.target.result;
                 const array = new Uint8Array(arrayBuffer);
-
+                // [TIPP #12] Nem kellenek olyan változók, amiket nem használunk (Itt nem elég onnan, hogy "new GBX({")
                 let mGBX = new GBX({
                     data: array,
                     onParse: function (metadata) {
@@ -33,11 +36,13 @@ function transformFiles() {
 }
 
 function verifyFiles() {
+    // [TIPP #14] Érdemes lenne csak az újonna beérkező fájlokat validálni és nem mindig az összeset
     console.log(fileList);
 
     for (file of fileList) {
         let fileType = file.name.split(".").pop();
         let validExtensions = ["Gbx"];
+        // [TIPP #15] Megeshet, hogy valaki által feltöltött fájl kisbetűs gbx kiterjesztésű és ezáltal fennakad a validáción
         if (!validExtensions.includes(fileType)) {
             let index = fileList.indexOf(file);
             fileList.splice(index, 1);
@@ -51,6 +56,8 @@ function verifyFiles() {
 function updateView() {
     dropArea.classList.remove("active");
     if (fileList.length > 0) {
+        // [TIPP #16] Kis szintaktikai varázslat, ha már template stringeket használsz
+        // filledText.textContent = `${fileList.length} file${fileList.length > 1 ? 's' :''} are ready to upload`
         if (fileList.length > 1) {
             filledText.textContent = `${fileList.length} files are ready to upload`;
         } else {
@@ -64,6 +71,14 @@ function updateView() {
     }
 }
 
+
+// [TIPP #13] Rossz megszokás az addEventListener ne nagyon használd
+//Inkább bindold rá az onClick eventet a domban az elementre (sokkal átláthatóbb a kód)
+//Az alábbi esetekben lehet érdemes használ az eventListenereket
+//1. Ha valamilyen logia alapján dinamikusan akarsz függvényeket hozzáadni egy elemhez
+//2. Ha egy DOM elemhez sok függvényt szeretnél hozzárendelni
+//3. Ha valami iteráció alapján akarsz hozzárendelni valamikhez függvényt (pl.: minden táblázat cellára egy onclicket)
+//4. Ha változik az a függvény valahol, amit hozzárendelsz egy adott elemhez
 browseButton.addEventListener("click", () => {
     input.click();
 });
--- public/table-fill.js
+++ public/table-fill.js
@@ -1,6 +1,10 @@
 
 const tableBody = document.querySelector("tbody");
 
+// [TIPP #6] Olyan függvényt amit több helyen is feltehetőleg használni fogunk
+//Pl itt adatok lekérése és táblázat frissítése érdemes egy külön függvénybe szervezni
+//(pl lehetne egy initTableContent() függvény amit meghív az initBody())
+//(Tudom, hogy ezt én írtam, de attól még lehet belőle okulni)
 function initBody() {
     fetch(`${HOST}/records`)
     .then(response => response.json())
@@ -15,7 +19,11 @@ function initBody() {
 //         .catch(err => console.log(err));
 // });
 
+// [TIPP #3] Lásd index.html
+// [TIPP #7] Kerülendőek a túl specifikus változónevek (data)
 function fillTable(data) {
+    //[TIPP #8] A változó névnek érdemes a változó tartalmára utalni
+    //Itt a row az adatbázisból érkező adatra utal és nem a táblázat egy sorára, ami félrevezető lehet.
     for (let row of data) {
         if (row.track.season == "Spring" && row.track.year == 2022) {
             var mapNumber = parseInt(row.track.map);
@@ -36,7 +44,7 @@ function fillTable(data) {
 
             if (playerNumber !== -1) {
                 var cell = tableBody.querySelector(`tr:nth-of-type(${mapNumber})`).querySelector(`td:nth-of-type(${playerNumber})`);
-
+                //[TIPP #9] Abszolút nem fontos, de errorra futhat a rendszer, ha órás nagyságrendig tart egy pálya lefutása
                 milliSec = row.time.toString().slice(-3);
                 secTemp = parseInt(row.time.toString().slice(0, -3));
                 min = Math.floor(secTemp / 60);
@@ -53,11 +61,11 @@ function formatTable(){
     let fastestCellInfo, currentTime, currentCell;
      for (var i = 0, row; row = tableBody.rows[i]; i++) {
         fastestCellInfo = { index:1, time:Infinity}
-        for (var j = 0, col; col = row.cells[j]; j++) {
-            if (!col.innerHTML.includes(':')) {
+        for (var j = 0, cell; cell = row.cells[j]; j++) {
+            if (!cell.innerHTML.includes(':')) {
                 continue;
             }
-            currentTime = parseFloat(col.innerHTML.replace(':',''))
+            currentTime = parseFloat(cell.innerHTML.replace(':',''))
             if(currentTime < fastestCellInfo.time){
               fastestCellInfo = { index:j, time:currentTime}
             }
